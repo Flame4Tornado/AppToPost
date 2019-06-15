2e3f88a0ab6818f8e2892674345f71ab34955d4e
Adam Gulmantowicz <adam.gulmantowicz@interia.pl>
2019-06-15 19:25:46 +0200

    search add

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const News = require('../models/news')
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('news', { title: 'News' });
+    const search = req.query.search || '';
+
+    const data = News
+    .find({title: new RegExp(search.trim(), 'i')})
+    .sort({created: -1});
+    data.exec((err, dataAc)=>{
+        res.render('news', {title: 'News', dataAc, search })
+    })
 });
+
+
 
 module.exports = router;
